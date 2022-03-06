52c40747cc0841e274ecbbe9b29d7dc3b16ea30f
Isaac Spiegel <isaacspiegel@gmail.com>
2022-03-06 12:14:43 -0600

    Show point on map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
 import GoogleMapReact from 'google-map-react';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import env from 'react-dotenv';
 
 /**
@@ -40,13 +40,44 @@ function Marker({
 export type MapProps = {
   selectedPlace: any;
   setSelectedPlace: any;
+  boundariesState: any;
+}
+
+function Point({
+  lat,
+  lng,
+}: MarkerProps) {
+  return (
+    <svg
+      viewBox="0 0 30 30"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        position: 'absolute',
+        //top: '0',
+        left: '-15px',
+      }}
+    >
+      <circle
+        cx="15"
+        cy="15"
+        r="15"
+        color="red"
+        fill="red"
+      />
+    </svg>
+  );
 }
 
 export default function Map({
   selectedPlace,
   setSelectedPlace,
+  boundariesState,
 }: MapProps) {
+  const [map, setMap] = useState<any>(null);
+  const [maps, setMaps] = useState<any>(null);
+
   const [geocoder, setGeocoder] = useState<any>(null);
+
   const [center, setCenter] = useState<any>({
     lat: 39.092306123688125,
     lng: -94.58670048764,
@@ -54,12 +85,65 @@ export default function Map({
 
   const [zoom, setZoom] = useState<number>(9);
 
+  const [boundaries, setBoundaries] = useState<any>(null);
+
+  const [point, setPoint] = useState<any>(null);
+
   const selectedLocation = useMemo(() => {
     return {
       lat: selectedPlace?.geometry?.location.lat() || null,
       lng: selectedPlace?.geometry?.location.lng() || null,
     };
   }, [selectedPlace]);
+
+  useEffect(() => {
+    if (boundariesState !== null && boundariesState !== boundaries) {
+      setBoundaries(boundariesState);
+      console.log(boundariesState);
+    }
+  }, [boundariesState, boundaries]);
+
+  useEffect(() => {
+    console.log('baoundary change');
+    console.log(!!map);
+    console.log(!!maps);
+    console.log(boundaries);
+    if (
+      !!map
+      && !!maps
+      && boundaries
+      && boundaries.data
+    ) {
+      console.log('boundaries', boundaries);
+      const boundaryData = boundaries.data.boundaries;
+      const type = boundaryData?.type;
+      if (type === 'Polygon') {
+        console.log('polygon');
+        const poly = new maps.Polygon({
+          paths: [
+            { lat: 39.092306123688125, lng: -94.58670048764 },
+            { lat: 39.093306123688125, lng: -94.58670048764 },
+            { lat: 39.093306123688125, lng: -94.68670048764 },
+          ],
+          strokeColor: "#FF0000",
+          strokeOpacity: 0.8,
+          strokeWeight: 2,
+          fillColor: "#FF0000",
+          fillOpacity: 0.35
+        });
+        poly.setMap(map);
+      } else if (type === 'Point') {
+        console.log('point');
+        setPoint({
+          lat: boundaryData?.coordinates[1],
+          lng: boundaryData?.coordinates[0],
+        });
+      }
+    }
+  }, [map, maps, boundaries]);
+
+  console.log('point', point);
+  console.log('!!point', !!point);
 
   return (
     <div style={{ height: 'calc(100vh - 72px)', width: '100%' }}>
@@ -70,6 +154,8 @@ export default function Map({
         }}
         onGoogleApiLoaded={({ map, maps }) => {
           setGeocoder(new maps.Geocoder());
+          setMap(map);
+          setMaps(maps);
         }}
         yesIWantToUseGoogleMapApiInternals={true}
         defaultCenter={{ lat: -34.397, lng: 150.644 }}
@@ -87,14 +173,8 @@ export default function Map({
           }
         }}
         onClick={({ event, x, y, lat, lng }) => {
-          console.log('event', event);
-          console.log('x', x);
-          console.log('y', y);
-          console.log('lat', lat);
-          console.log('lng', lng);
+          console.log('click', event, x, y, lat, lng);
           geocoder && geocoder.geocode({ location: { lat, lng } }, (results: any, status: any) => {
-            console.log('results', results);
-            console.log('status', status);
             setSelectedPlace(results[0]);
             setCenter({
               lat: selectedPlace?.geometry?.location.lat(),
@@ -107,6 +187,12 @@ export default function Map({
           <Marker
             lat={selectedLocation.lat}
             lng={selectedLocation.lng}
+          />
+        )}
+        {point?.lat && point?.lng && (
+          <Point
+            lat={point.lat}
+            lng={point.lng}
           />
         )}
       </GoogleMapReact>
